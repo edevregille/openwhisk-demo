ee4bcf214de7124098bafcde0e63180bcb164e6f
edevregille <manudevregille@gmail.com>
2016-03-28 19:15:43 +0100

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,27 +1,34 @@
-var request = require('request');
+function main(param) {
+	
+	var request = require('request');
 
-function main(params) {
 	var oauth  ={
 		consumer_key:'',
 		consumer_secret:'',
 		token:'',
 		token_secret:''
 	};
-	var tweet = params.twit || 'OpenWhisk Action - Change detected in Cloudant';
+	
+	var tweet = param.text || 'OpenWhisk Action - Change detected in Cloudant';
 	var url = 'https://api.twitter.com/1.1/statuses/update.json?status='+ tweet;
 
 	var result;
+
+	//send tweet - post twitter status API v1.1.
 	request.post({
 		url: url, 
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
 		oauth: oauth
 	}, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
+		if (!error && response.statusCode== 200) {
 			result = response ; 
 		}
 		else {
 			result= error;
 		}
-		whisk.done({msg:result});
+		whisk.done({msg:response});
 	});	
 	return whisk.async();
 }
